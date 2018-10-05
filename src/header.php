8cca6d1bb3c86fe2eb8c6aca061ebbf1c18d9294
ZHU Haoran <haoran.zhu@isen.yncrea.fr>
2018-10-05 17:25:10 +0200

    mock_up

--- src/header.php
+++ src/header.php
@@ -6,7 +6,7 @@
 <ul>
 <li><a href="index.php">Homepage</a></li>
 <li><a href="login.php">Login</a></li>
-<li><a href="produire.php">Product</a></li>
+<li><a href="produire.php">Products</a></li>
 <li><a href="cart.php">Cart</a></li>
 </ul>
 </nav>
